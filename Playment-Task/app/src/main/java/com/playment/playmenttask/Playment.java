8023fc9c0314461a5e87cd464e9ee5ba9061e20f
Shekhar <36541597+kunwarshekhar@users.noreply.github.com>
2018-06-06 21:36:16 +0530

    App: Initailize the project
    Initialization of the application development

---
+++ Playment-Task/app/src/main/java/com/playment/playmenttask/Playment.java
@@ -0,0 +1,13 @@
+package com.playment.playmenttask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Playment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playment);
+    }
+}
